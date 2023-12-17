c27af876b81acd569ea61a6bd25c3dd15ea9e505
myevilpumpkin <dima.tomin2010@yandex.ru>
2023-12-17 06:30:40 +0300

    Added calendar

---
+++ src/modules/tasks_handler.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import pandas as pd
+
+from src.ical_tasks.tasks import get_tasks_as_df
+
+
+def get_all_tasks() -> pd.DataFrame:
+    return get_tasks_as_df()
+
+
+def get_actual_tasks() -> pd.DataFrame:
+    tasks = get_all_tasks()
+    actual_mark = tasks['end'].dt.date >= datetime.now().date()
+    return tasks[actual_mark]
---
+++ src/tasks_viewer_app.py
@@ -0,0 +1,19 @@
+import sys
+
+import streamlit as st
+from streamlit.web import cli as stcli
+
+from modules.tasks_handler import get_actual_tasks
+
+
+def main():
+    st.title('Tasks Viewer')
+    st.write(get_actual_tasks())
+
+
+if __name__ == '__main__':
+    if st.runtime.exists():
+        main()
+    else:
+        sys.argv = ['streamlit', 'run', sys.argv[0]]
+        sys.exit(stcli.main())
