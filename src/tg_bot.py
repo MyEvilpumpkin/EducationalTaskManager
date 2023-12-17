f4de14766ea31930fd15e22b5725ab77716431f8
myevilpumpkin <dima.tomin2010@yandex.ru>
2023-12-18 01:40:37 +0300

    Updated tg bot logging

--- src/tg_bot.py
+++ src/tg_bot.py
@@ -5,7 +5,7 @@ from telegram_bot import application
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.WARNING
 )
 
 if __name__ == '__main__':
